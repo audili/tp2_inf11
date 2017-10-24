9b715cdb71144a664b202444eb3d6e1c78a50231
ap12220 <a.joudad@gmail.com>
2017-10-24 14:04:08 -0400

    nouvelle classe test

---
+++ tp2_inf/src/tp2_inf/test.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package tp2_inf;
+
+/**
+ * @author ap12220
+ *
+ */
+public class test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
