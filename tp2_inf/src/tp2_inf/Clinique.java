afcf75c97c6db62388c0e9e9c9813bb1e9fb1373
ap12220 <a.joudad@gmail.com>
2017-10-24 16:20:57 -0400

    classe clinique incomplète parce qu'il manque la classe calendrier et pour calendrier il faut poser une question au professeur

---
+++ tp2_inf/src/tp2_inf/Clinique.java
@@ -0,0 +1,52 @@
+package tp2_inf;
+
+
+import java.util.LinkedList;
+
+public class Clinique {
+
+ private LinkedList<Docteur> listeDocteur = new LinkedList<Docteur>();
+ private LinkedList<Infirmier> listeInfirmier = new LinkedList<Infirmier>();
+ private LinkedList<Patient> listePatient = new LinkedList<Patient>();
+ 
+public LinkedList<Docteur> getListeDocteur() {
+	
+	return listeDocteur;
+	
+}
+public void setListeDocteur(LinkedList<Docteur> listeDocteur) {
+	
+	this.listeDocteur = listeDocteur;
+	
+}
+public LinkedList<Infirmier> getListeInfirmier() {
+	
+	return listeInfirmier;
+	
+}
+
+
+public void setListeInfirmier(LinkedList<Infirmier> listeInfirmier) {
+	
+	this.listeInfirmier = listeInfirmier;
+	
+}
+public LinkedList<Patient> getListePatient() {
+	
+	return listePatient;
+	
+}
+public void setListePatient(LinkedList<Patient> listePatient) {
+	
+	this.listePatient = listePatient;
+	
+}
+
+ 
+ 
+ 
+ 
+ 
+
+	
+}
