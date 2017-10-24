e85a31500631968e01e0d8cc91ce48e55f921271
ayoubmoudrika <ak37700@SEG-E4021-26.ens.ad.etsmtl.ca>
2017-10-24 14:59:42 -0400

    début du main

---
+++ tp2_inf/src/tp2_inf/Main.java
@@ -0,0 +1,52 @@
+package tp2_inf;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner clavier = new Scanner(System.in);
+
+		System.out.print("Bienvenue à la clinique\nQue voulez-vous faire ?\n1) Ajouter un docteur\n2) Ajouter un infirmier\n3) Ajouter un patient\n4) Ajouter un rendez-vous"
+				+ "\n5) Trouver un rendez-vous pour un patient\n6) Afficher le prochain rendez-vous d'un docteur\n7) Afficher le prochain rendez-vous d'un infirmier\n"
+				+ "8) Afficher le prochain rendez-vous d'un patient\n9) Passer à la prochaine plage horaire\n10) Afficher le calendrier complet\n11) Afficher le calendrier complet d'un docteur\n"
+				+ "12) Afficher le calendrier complet d'un infirmier\n13) Annuler un rendez-vous\n14) Quitter\n ");
+		int choix = clavier.nextInt();
+
+		switch (choix){
+		
+		case 1:
+			
+		
+		case 2:
+			
+		case 3:
+			
+		case 4:
+			
+		case 5:
+			
+		case 6:
+			
+		case 7:
+			
+		case 8:
+			
+		case 9:
+			
+		case 10:
+			
+		case 11:
+			
+		case 12:
+			
+		case 13:
+		
+		case 14:
+
+		
+		}
+
+	}
+
+}
